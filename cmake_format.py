27eb8abae654d8586bb0d554e1602cd0697242ed
Josh Bialkowski <josh@skyd.io>
2016-03-11 13:31:40 -0800

    lint

--- cmake_format.py
+++ cmake_format.py
@@ -1,19 +1,24 @@
 """Parse cmake listfiles and format them nicely."""
 
 import argparse
-import cmakelists_parsing.parsing as cmp
+import cmakelists_parsing.parsing as cmparse
 import re
 import shutil
 import sys
 import tempfile
 import textwrap
 
+
 class AttrDict(dict):
+    """Access elements of a dictionary as attributes."""
+
     def __init__(self, *args, **kwargs):
         super(AttrDict, self).__init__(*args, **kwargs)
         self.__dict__ = self
 
+
 def build_attr_dict_r(regular_dict):
+    """Recursively construct an AttrDict from a regular one."""
     attr_dict = AttrDict()
     for key, value in regular_dict.iteritems():
         if isinstance(value, dict):
@@ -32,16 +37,17 @@ DEFAULT_CONFIG = build_attr_dict_r(dict(
     line_width=80,
     tab_size=2,
     max_subargs_per_line=3,
-    ))
+))
+
 
 def format_comment_block(config, line_width, comment_lines):
-    """Reflow a comment block into the given line_width. Return a list of 
+    """Reflow a comment block into the given line_width. Return a list of
        lines."""
     stripped_lines = [line[1:].strip() for line in comment_lines]
 
     paragraph_lines = list()
     paragraphs = list()
-    # A new "paragraph" starts at a paragraph boundary (double newline), or at 
+    # A new "paragraph" starts at a paragraph boundary (double newline), or at
     # the start of a TODO(...): or NOTE(...):
     for line in stripped_lines:
         if NOTE_REGEX.match(line):
@@ -71,6 +77,7 @@ def format_comment_block(config, line_width, comment_lines):
         lines.extend(wrapper.wrap(paragraph_text))
     return lines
 
+
 def split_args_by_kwargs(args):
     """Takes in a list of arguments and returns a lists of lists. Each sublist
        starts with a kwarg (ALL_CAPS) and contains only non-kwargs after."""
@@ -86,6 +93,7 @@ def split_args_by_kwargs(args):
 
     return arg_split
 
+
 def arg_exists_with_comment(args):
     """Return true if any arg in the arglist contains a comment."""
     for arg in args:
@@ -93,14 +101,15 @@ def arg_exists_with_comment(args):
             return True
     return False
 
+
 def format_single_arg(config, line_width, arg):
     """Return a list of lines that reflow the single arg and all it's comments
        into a block with width at most line_width."""
     if arg.comments:
-        comment_stream = ' '.join([comment[1:].strip() 
+        comment_stream = ' '.join([comment[1:].strip()
                                    for comment in arg.comments])
         initial_indent = arg.contents + ' # '
-        subsequent_indent = ' '*len(arg.contents) + ' # '
+        subsequent_indent = ' ' * len(arg.contents) + ' # '
         wrapper = textwrap.TextWrapper(width=line_width,
                                        expand_tabs=True,
                                        replace_whitespace=True,
@@ -111,8 +120,9 @@ def format_single_arg(config, line_width, arg):
     else:
         return [arg.contents]
 
+
 def format_arglist(config, line_width, args):
-    """Given a list arguments containing at most one KWARG (in position [0] 
+    """Given a list arguments containing at most one KWARG (in position [0]
        if it exists), format into a list of lines."""
     if len(args) < 1:
         return []
@@ -120,7 +130,7 @@ def format_arglist(config, line_width, args):
     if KWARG_REGEX.match(args[0].contents):
         kwarg = args[0].contents
         lines = [kwarg]
-        indent_str = ' '*(len(kwarg) + 1)
+        indent_str = ' ' * (len(kwarg) + 1)
         args.pop(0)
     else:
         indent_str = ''
@@ -129,11 +139,12 @@ def format_arglist(config, line_width, args):
     for arg in args:
         # Lines to add if we were to put the arg at the end of the current
         # line.
-        lines_append = format_single_arg(config, 
+        lines_append = format_single_arg(config,
                                          line_width - len(lines[-1]) - 1, arg)
 
         # Lines to add if we are going to make a new line for this arg
-        lines_new = format_single_arg(config, line_width - len(indent_str), arg)
+        lines_new = format_single_arg(
+            config, line_width - len(indent_str), arg)
 
         # If going to a new line greatly reduces the number of lines required
         # then choose that option over the latter.
@@ -142,7 +153,7 @@ def format_arglist(config, line_width, args):
             for line in lines_new:
                 lines.append(indent_str + line)
         else:
-            arg_indent_str = ' '*(len(lines[-1]) + 1)
+            arg_indent_str = ' ' * (len(lines[-1]) + 1)
             if len(lines[-1]) > 0:
                 lines[-1] += ' '
             lines[-1] += lines_append[0]
@@ -152,13 +163,14 @@ def format_arglist(config, line_width, args):
 
     return lines
 
+
 def format_args(config, line_width, args):
     """Format arguments into a block with at most line_width chars."""
     if not arg_exists_with_comment(args):
         single_line = ' '.join([arg.contents for arg in args])
         if len(single_line) < line_width:
             return [single_line]
-    
+
     lines = []
     arg_multilist = split_args_by_kwargs(args)
     for arg_sublist in arg_multilist:
@@ -166,57 +178,62 @@ def format_args(config, line_width, args):
         lines.extend(sublist_lines)
     return lines
 
+
 def format_command(config, command, line_width):
     """Formats a cmake command call into a block with at most line_width chars.
        Returns a list of lines."""
-    
+
     command_start = command.name + '('
 
     # If there are no args then return just the command
     if len(command.body) < 1:
         return [command_start + ')']
     else:
-        # Format args into a block that is aligned with the end of the 
+        # Format args into a block that is aligned with the end of the
         # parenthesis after the command name
-        lines_a = format_args(config, line_width - len(command_start), 
+        lines_a = format_args(config, line_width - len(command_start),
                               command.body)
 
         # Format args into a block that is aligned with the command start
         # plus one tab size
         lines_b = format_args(config,
                               line_width - config.tab_size, command.body)
-        
+
         # TODO(josh) : handle inline comment for the command
         # If the version aligned with the comand start + indent has *alot*
         # fewer lines than the version aligned with the command end, then
         # use this one
         if len(lines_a) > 4 * len(lines_b):
             lines = [command_start]
-            indent_str = ' '*config.tab_size
+            indent_str = ' ' * config.tab_size
             for line in lines_b:
                 lines.append(indent_str + line)
-            if(len(lines[-1]) < line_width):
+            if len(lines[-1]) < line_width:
                 lines[-1] += ')'
             else:
                 lines.append(indent_str[:-1] + ')')
-            
+
         # Otherwise use the version that is alinged with the command ending
         else:
             lines = [command_start + lines_a[0]]
-            indent_str = ' '*len(command_start)
+            indent_str = ' ' * len(command_start)
             for line in lines_a[1:]:
                 lines.append(indent_str + line)
-            if(len(lines[-1]) < line_width):
+            if len(lines[-1]) < line_width:
                 lines[-1] += ')'
             else:
                 lines.append(indent_str[:-1] + ')')
     return lines
 
+
 def write_indented(outfile, indent_str, lines):
+    """Write lines to outfile prefixed with indent_str."""
+
     for line in lines:
         outfile.write(indent_str)
         outfile.write(line)
         outfile.write('\n')
+
 
 class PrettyPrinter(object):
     """Manages state during processing of file lines. Accumulates newlines and
@@ -232,37 +249,44 @@ class PrettyPrinter(object):
         self.blank_parts = list()
 
     def flush_blanks(self):
+        """Consume a string of blank lines and write out a single one."""
+
         if self.blank_parts:
             self.outfile.write('\n')
             self.blank_parts = list()
 
     def flush_comment(self):
+        """Consume a string of comment lines, reflow, and write out."""
+
         if self.comment_parts:
-            indent_str = ' '*(self.config.tab_size*self.scope_depth)
-            lines = format_comment_block(self.config, 
-                                         self.config.line_width 
-                                            - len(indent_str), 
+            indent_str = ' ' * (self.config.tab_size * self.scope_depth)
+            lines = format_comment_block(self.config,
+                                         self.config.line_width
+                                         - len(indent_str),
                                          self.comment_parts)
             write_indented(self.outfile, indent_str, lines)
             self.comment_parts = list()
 
     def consume_part(self, part):
-        if isinstance(part, cmp.BlankLine):
+        """Consume a single parsed object."""
+
+        if isinstance(part, cmparse.BlankLine):
             self.flush_comment()
             self.blank_parts.append(part)
-        elif isinstance(part, cmp.Comment):
+        elif isinstance(part, cmparse.Comment):
             self.flush_blanks()
             self.comment_parts.append(part)
 
-        elif isinstance(part, cmp._Command):
+
+        elif isinstance(part, cmparse._Command): # pylint: disable=protected-access
             self.flush_comment()
             self.flush_blanks()
             command = part
             if command.name in SCOPE_DECREASE:
                 self.scope_depth -= 1
 
-            indent_str = ' '*(self.config.tab_size*self.scope_depth)
-            lines = format_command(self.config, command, 
+            indent_str = ' ' * (self.config.tab_size * self.scope_depth)
+            lines = format_command(self.config, command,
                                    self.config.line_width - len(indent_str))
             write_indented(self.outfile, indent_str, lines)
 
@@ -273,10 +297,13 @@ class PrettyPrinter(object):
             raise ValueError('Unrecognized parse type {}'.format(type(part)))
 
     def consume_parts(self, parsed_listfile):
+        """Consume a list of parsed cmake objects."""
+
         for part in parsed_listfile:
             self.consume_part(part)
         self.flush_comment()
         self.flush_blanks()
+
 
 def process_file(config, infile, outfile):
     """Iterates through lines in the file and watches for cmake_format on/off
@@ -289,9 +316,9 @@ def process_file(config, infile, outfile):
     for line in iter(infile.readline, b''):
         if active:
             if line.find('cmake_format: off') != -1:
-                parsed_listfile = cmp.parse(format_me)
+                parsed_listfile = cmparse.parse(format_me)
                 pretty_printer.consume_parts(parsed_listfile)
-                parsed_listfile = cmp.parse(line)
+                parsed_listfile = cmparse.parse(line)
                 pretty_printer.consume_parts(parsed_listfile)
                 format_me = ''
                 active = False
@@ -300,7 +327,7 @@ def process_file(config, infile, outfile):
         else:
 
             if line.find('cmake_format: on') != -1:
-                parsed_listfile = cmp.parse(line)
+                parsed_listfile = cmparse.parse(line)
                 pretty_printer.consume_parts(parsed_listfile)
                 active = True
                 format_me = ''
@@ -308,10 +335,13 @@ def process_file(config, infile, outfile):
                 outfile.write(line)
 
     if format_me:
-        parsed_listfile = cmp.parse(format_me)
+        parsed_listfile = cmparse.parse(format_me)
         pretty_printer.consume_parts(parsed_listfile)
 
+
 def main():
+    """Parse arguments, open files, start work."""
+
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-i', '--in-place', action='store_true')
     parser.add_argument('-o', '--outfile-path', default='-')
@@ -320,6 +350,7 @@ def main():
     parser.add_argument('infilepaths', nargs='+')
     args = parser.parse_args()
 
+    # pylint: disable=attribute-defined-outside-init
     config = DEFAULT_CONFIG
     config.line_width = args.line_width
     config.tab_size = args.tab_size
